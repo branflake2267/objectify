f8407ab7ee4bf303b613b4046de9086939cc76c2
Brandon Donnelson <branflake2267@gmail.com>
2013-01-05 00:53:14 -0800

    I got a basic test setup but I'm having trouble with app engine source during test, which I haven't had on other projects.

--- objectify-gwt-testing/src/main/java/com/googlecode/objectify/server/RpcServiceImpl.java
+++ objectify-gwt-testing/src/main/java/com/googlecode/objectify/server/RpcServiceImpl.java
@@ -1,11 +1,10 @@
 package com.googlecode.objectify.server;
 
-import static com.googlecode.objectify.ObjectifyService.ofy;
-
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
-import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.Key;
 import com.googlecode.objectify.Ref;
 import com.googlecode.objectify.client.rpc.RpcService;
+import com.googlecode.objectify.impl.ref.StdRef;
 import com.googlecode.objectify.shared.domain.Contact;
 import com.googlecode.objectify.shared.domain.Person;
 
@@ -15,29 +14,22 @@ import com.googlecode.objectify.shared.domain.Person;
 @SuppressWarnings("serial")
 public class RpcServiceImpl extends RemoteServiceServlet implements RpcService {
 
-  static {
-    ObjectifyService.register(Person.class);
-    ObjectifyService.register(Contact.class);
-  }
+//  static {
+//    ObjectifyService.register(Person.class);
+//    ObjectifyService.register(Contact.class);
+//  }
   
   public Person getPerson() {
     Person person = new Person();
+    person.setName("name");
     
-    // TODO throw it through the rpc
-    try {
-      person.setName("testing");
-      
-      Contact contact = new Contact();
-      contact.setName("testing-ref");
-      ofy().save().entity(contact).now();
-      
-      Ref<Contact> refContact = Ref.create(contact);
-      person.setContact(refContact);
-      
-      ofy().save().entity(person).now();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+    Contact contact = new Contact();
+    contact.setId(1l);
+    contact.setName("name");
+    
+    Key<Contact> keyContact = Key.create(Contact.class, 1);
+    Ref<Contact> refContact = StdRef.create(keyContact, contact);
+    person.setContact(refContact);
      
     return person;
   }
